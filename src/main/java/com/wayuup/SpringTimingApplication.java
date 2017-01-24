5243de6a8b8c45c8a8ae623b02b034fb461945bf
Ethel Salazar <Ethel Salazar>
2017-01-23 20:48:59 -0600

    project template

---
+++ src/main/java/com/wayuup/SpringTimingApplication.java
@@ -0,0 +1,12 @@
+package com.wayuup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTimingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTimingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wayuup/SpringTimingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wayuup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTimingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
